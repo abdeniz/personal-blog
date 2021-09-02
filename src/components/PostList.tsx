5bd31c05f669e731e006cd04d8dc6fddc6925531
Deniz Abdurrahmani <abdurrahmani.deniz00@gmail.com>
2021-09-02 21:44:47 +0300

    Removed unnecessary imports

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import Main from './Main'
 import PostListItem, { IPostListItem } from './PostListItem'
 
 const tempPosts: IPostListItem[] = [
